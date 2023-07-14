052085056815833d3ade67c3110b710048ea5d22
Nandiniperala <137399369+Nandiniperala@users.noreply.github.com>
2023-07-14 15:19:10 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+    {
+      name: "Na roja nuvve",
+      artist: "Hesham Abdul Wahab",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ye katha etu vellthundho",
+      artist: "Ramajogayya Sastry",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Undiporaadhey",
+      artist: "Sid sriram",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Padam etu potunna",
+      artist: "Karthik",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Life is beautiful",
+      artist: "Karthik",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Mataru Mataru",
+      artist: "Shweta Mohan",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
